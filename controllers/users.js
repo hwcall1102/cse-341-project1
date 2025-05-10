1909185906e7f9e2686025527c385556ba135e6a
hwcall1102 <haydenc0293@gmail.com>
2025-05-10 13:43:32 -0600

    through step 5

---
+++ controllers/users.js
@@ -0,0 +1,21 @@
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+    const result = await mongodb.getDb().db().collection('ContactsList').find();
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/JSON');
+        res.status(200).json(users);
+    })    
+}
+
+const getSingle = async (req, res) => {
+    const userId = new ObjectId(req.params.id)
+    const result = await mongodb.getDb().db().collection('ContactsList').find({_id: userId});
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/JSON');
+        res.status(200).json(users[0]);
+    })    
+}
+
+module.exports = { getAll, getSingle}
