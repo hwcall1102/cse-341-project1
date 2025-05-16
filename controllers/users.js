035452b0f63acd4811c4bfaf99ad52efb17acd38
hwcall1102 <haydenc0293@gmail.com>
2025-05-15 20:42:23 -0600

    step 1

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ const getAll = async (req, res) => {
         res.setHeader('Content-Type', 'application/JSON');
         res.status(200).json(users);
     })    
-}
+};
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id)
@@ -16,6 +16,50 @@ const getSingle = async (req, res) => {
         res.setHeader('Content-Type', 'application/JSON');
         res.status(200).json(users[0]);
     })    
-}
+};
 
-module.exports = { getAll, getSingle}
+const createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db().collection('ContactList').insertOne(user);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while creating the user');
+    };
+};
+
+const updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db().collection('ContactList').replaceOne({ _id: userId }, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while updating the user');
+    };
+};
+
+const deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('ContactList').deleteOne({ _id: userId });
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while deleting the user');
+    };
+};
+
+
+module.exports = { getAll, getSingle, createUser, updateUser, deleteUser };
